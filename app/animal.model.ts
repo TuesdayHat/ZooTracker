b12e1e24f69a4254521010cf5a413c9c2883fcf8
Guest User <Guest@Epicodus-4C.local>
2018-02-23 09:12:35 -0800

    implement Angular template

---
+++ app/animal.model.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'app-root',
+  template:`
+  <div class="container">
+  </div>
+  `
+})
+
+export class AppComponent {}
